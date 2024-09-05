beb414e82188806c9ca8bb9161d1a3b3f7509959
KirillBunakov <lonley987@gmail.com>
2024-09-05 18:02:53 +0300

    fixed drinkbar styles

--- src/components/modules/Cafe/DrinkBar.vue
+++ src/components/modules/Cafe/DrinkBar.vue
@@ -18,9 +18,9 @@ const drinkTypes = ref([
   <section class="" :class="{ сoffee: activeDrink === 'Кофе', tea: activeDrink === 'Чай', milk: activeDrink === 'Молочный коктейль', water: activeDrink === 'Морсы и газ. напитки'}" >
     <div class="grid grid-cols-4 items-start">
       <div class="flex flex-col items-center h-96 relative first:rounded-tl-4xl last:rounded-tr-4xl grey" v-for="drink in drinkTypes" :key="drink.name" @click="$emit('currentDrink',drink.name)" :class="{ activeCard : activeDrink === drink.name}">
-        <img :src="drink.img" alt="">
-        <p class="mont text-3xl font-medium text-center w-64">{{ drink.name }}</p>
-        <div class="w-10 h-5 absolute -bottom-28 triangle" v-if="activeDrink === drink.name"></div>
+        <img :src="drink.img" alt="" class="h-64 w-64">
+        <p class="mont text-3xl font-medium text-center w-52">{{ drink.name }}</p>
+        <div class="w-10 h-5 absolute -bottom-24 triangle" v-if="activeDrink === drink.name"></div>
       </div>
     </div>
   </section>
